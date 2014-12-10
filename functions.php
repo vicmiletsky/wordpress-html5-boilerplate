f051b647a77e58af7e968ba1bda153ea5765df7f
Victor <victor.miletskiy@gmail.com>
2014-12-10 20:16:41 +0200

    Small fix to functions.php

--- functions.php
+++ functions.php
@@ -1,15 +1,15 @@
 <?php
 
-/**
- * Load all stuff from ./functions dir
- */
+// Load all stuff from ./functions dir
 
-$files = glob(__DIR__ . '/functions/*.php');
+require_once __DIR__ . '/functions/utils.php';
+require_once __DIR__ . '/functions/actions.php';
 
+/*$files = glob(__DIR__ . '/functions/*.php');
 if($files) {
     foreach($files as $file) {
         if(is_file($file)) {
             require_once $file;
         }
     }
-}
+}*/
