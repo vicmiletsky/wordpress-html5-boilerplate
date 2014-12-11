c774ca6e7053254f0750036ee50c5148944aab25
Victor <victor.miletskiy@gmail.com>
2014-12-11 21:06:59 +0200

    Libraries updated to more recent versions

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="<?= assets_dir() ?>/js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="<?= assets_dir() ?>/js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
     <script src="<?= assets_dir() ?>/js/main.js"></script>
 
     <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
